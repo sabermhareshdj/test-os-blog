d380ae5f0cdddb0f87b7cfa9c5e52460712eb6ea
Saber Mharesh <dj.saber.mharesh@gmail.com>
2023-10-26 17:01:27 +0300

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=30000)
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     draft = models.BooleanField(default=True)
     tags = TaggableManager()
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True,related_name='post_user')
@@ -22,3 +22,11 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='comment_author')
+    post = models.ForeignKey(Post,related_name='comment_post',on_delete=models.CASCADE)
+    comment = models.TextField(max_length=200)
+    create_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return str(self.user)
