c7fa40dd56a5e3ffd4284454caf09a8bc1c894f0
Saber Mharesh <dj.saber.mharesh@gmail.com>
2023-10-26 12:54:45 +0300

    add class based views

--- blog/views.py
+++ blog/views.py
@@ -1,48 +1,32 @@
 from django.shortcuts import render , redirect
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
+
+
 # Create your views here.
 
 
-def post_list(request):
-    data = Post.objects.all()
-    return render(request,'post_list.html',{'posts':data}) #context
+class PostList(ListView): #django make tmplate auto = post_list name data and action (list) "defult django project"
+    model = Post          # context ===> post_list , or object_list   "defult django project"
 
-def post_detail(request,post_id):
-    data = Post.objects.get(id=post_id)
-    return render(request,'post_detail.html',{'post':data})
+class PostDetail(DetailView):
+    model = Post
 
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','content','image','tags','create_date','draft','author']
+    success_url ='/blog'
 
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-            return redirect('/blog')
-    else:
-        form = PostForm()
-    return render(request,'new_post.html',{'form':form})
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title','content','image','tags','create_date','draft','author']
+    success_url ='/blog'
+    template_name = 'blog/edit_post.html'
 
+class PostDelete(DeleteView):
+    model = Post
+    success_url ='/blog'
 
-def edit_post(request,post_id):
-    data = Post.objects.get(id=post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES,instance=data)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-            return redirect('/blog')
-    else:
-        form = PostForm(instance=data)
-    return render(request,'edit_post.html',{'form':form})
-
-
-def delete_post(request,post_id):
-    data = Post.objects.get(id=post_id)
-    data.delete()
-    return redirect('/blog')
 
 
