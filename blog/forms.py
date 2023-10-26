669405300ceb2e36e4b8b3786216a60b553c12af
Saber Mharesh <dj.saber.mharesh@gmail.com>
2023-10-26 18:01:26 +0300

    add app comment

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from .models import Post
+from .models import Post , Comment
 
 
 class PostForm(forms.ModelForm):
@@ -8,4 +8,7 @@ class PostForm(forms.ModelForm):
         # fields = '__all__'
         exclude = ('author',)
             
-    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
-from .models import Post
-from .forms import PostForm
+from .models import Post , Comment
+from .forms import PostForm , CommentForm
 from django.views.generic import ListView , DetailView , CreateView , UpdateView , DeleteView
 
 
@@ -14,7 +14,18 @@ class PostList(ListView): #django make tmplate auto = post_list name data and ac
 #    model = Post
 def post_detail(request,pk):
     data = Post.objects.get(id=pk)
-    return render(request,'blog/post_detail.html',{'post':data})
+    post_comments = Comment.objects.filter(post=data)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.user = request.user
+            myform.post = data
+            myform.save()
+    else:
+        form = CommentForm()
+    return render(request,'blog/post_detail.html',{'post':data , 'form':form , 'post_comments':post_comments})
 
 
 
